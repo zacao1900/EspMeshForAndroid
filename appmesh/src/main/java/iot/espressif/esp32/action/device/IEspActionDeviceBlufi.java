402941d11822b301947b07923d2163eb6584a72f
liugang2019 <liugang@hualaikeji.com>
2020-03-12 14:40:01 +0800

    1.适配北京平台 修改service uuid

--- appmesh/src/main/java/iot/espressif/esp32/action/device/IEspActionDeviceBlufi.java
+++ appmesh/src/main/java/iot/espressif/esp32/action/device/IEspActionDeviceBlufi.java
@@ -10,7 +10,8 @@ import iot.espressif.esp32.model.device.ble.MeshBlufiCallback;
 import iot.espressif.esp32.model.device.ble.MeshBlufiClient;
 
 public interface IEspActionDeviceBlufi extends IEspActionDevice {
-    UUID UUID_SERVICE = UUID.fromString("0000ffff-0000-1000-8000-00805f9b34fb");
+//    UUID UUID_SERVICE = UUID.fromString("0000ffff-0000-1000-8000-00805f9b34fb");
+    UUID UUID_SERVICE = UUID.fromString("0000fd7b-0000-1000-8000-00805f9b34fb");
     UUID UUID_WRITE_CHARACTERISTIC = UUID.fromString("0000ff01-0000-1000-8000-00805f9b34fb");
     UUID UUID_NOTIFICATION_CHARACTERISTIC = UUID.fromString("0000ff02-0000-1000-8000-00805f9b34fb");
 
